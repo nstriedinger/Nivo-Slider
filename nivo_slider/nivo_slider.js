238cf3994cc709796599a67150d6a9f042ddf25c
Nathan Striedinger <nathan00@gmail.com>
2010-03-27 09:41:32 -0500

    add features

--- nivo_slider/nivo_slider.js
+++ nivo_slider/nivo_slider.js
@@ -1,16 +1,6 @@
 Drupal.behaviors.nivo_slider = function(context) {
-	$settings = Drupal.settings.nivo_slider;
-	  $('#slider').nivoSlider({
-			effect:$settings.effect,
-			slices:15,
-			animSpeed:$settings.animSpeed,
-			pauseTime:3000,
-			directionNav:false, //Next & Prev
-			directionNavHide:true, //Only show on hover
-			controlNav:true, //1,2,3...
-			pauseOnHover:true, //Stop animation while hovering
-			beforeChange: function(){},
-			afterChange: function(){}
-		});
-
+  // For each of our sliders provide the settings
+  $.each(Drupal.settings.nivo_slider, function(id) {
+	  $('#'+id).nivoSlider(this);
+  });
 };
--- nivo_slider/views-view-nivo-slider.tpl.php
+++ nivo_slider/views-view-nivo-slider.tpl.php
@@ -1,4 +1,4 @@
-<div id="slider" class="nivoSlider clearfix">
+<div id="<?php print $nivo_slider_id ?>" class="nivoSlider clearfix">
     <?php foreach ($rows as $row): ?>
       <?php print $row ?>
     <?php endforeach; ?>
